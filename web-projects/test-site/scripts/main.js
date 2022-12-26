2b62abfb70136749c4a8e2a6a22fd65b090d1269
wwwnormal <109011717+wwwnormal@users.noreply.github.com>
2022-12-26 17:20:29 +0800

    Add files via upload

---
+++ web-projects/test-site/scripts/main.js
@@ -0,0 +1,40 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+
+    let mySrc = myImage.getAttribute('src');
+
+    if(mySrc === 'images/IMG_5566-3.jpg') {
+      myImage.setAttribute('src', 'images/IMG_5566-4.jpg');
+    } else {
+      myImage.setAttribute('src', 'images/IMG_5566-3.jpg');
+    }
+
+}
+
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('请输入你的名字。');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+  }
+}
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+  }
+  
+  myButton.onclick = function() {
+    setUserName();
+ }
+ 
+  
